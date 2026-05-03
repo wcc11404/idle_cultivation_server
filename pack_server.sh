bc912f54f9ee3f785429f9c532ff7e3d968b6d1f
wcc11404 <chengyu_3663@126.com>
2026-05-03 12:50:18 +0800

    chore: slim server packaging output

--- pack_server.sh
+++ pack_server.sh
@@ -7,8 +7,8 @@ OUTPUT_TAR="$SCRIPT_DIR/idle_cultivation_server.tar"
 
 echo "开始打包服务端目录..."
 echo "输出文件: $OUTPUT_TAR"
-echo "排除目录: venv, .git, __pycache__, .pytest_cache"
-echo "排除文件: server.log, idle_cultivation_server.tar"
+echo "排除目录: venv, .git, __pycache__, .pytest_cache, ops_web/node_modules, ops_web/.npm-cache"
+echo "排除文件: server.log, test_server.log, idle_cultivation_server.tar"
 
 rm -f "$OUTPUT_TAR"
 
@@ -19,7 +19,10 @@ tar \
   --exclude='./.pytest_cache' \
   --exclude='./__pycache__' \
   --exclude='*/__pycache__' \
+  --exclude='./ops_web/node_modules' \
+  --exclude='./ops_web/.npm-cache' \
   --exclude='./server.log' \
+  --exclude='./test_server.log' \
   --exclude='./idle_cultivation_server.tar' \
   -cf "$OUTPUT_TAR" \
   .
